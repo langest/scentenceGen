5180b0585f8ba93cfa5931b3bdc62abb7441520e
Daniel Langesten <daniel.langest@gmail.com>
2018-10-07 09:25:19 +0200

    Adding functions for probabilities

--- main.go
+++ main.go
@@ -2,18 +2,50 @@ package main
 
 import (
 	"bufio"
-	"os"
 	"log"
+	"os"
+	"strings"
+)
+
+var (
+	bigramProbabilityMap map[string]map[string]int = make(map[string]map[string]int)
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	line, isPrefix, err := reader.ReadLine()
 	for err == nil {
-		log.Println(string(line))
+		previousWord := ""
+		for _, word := range strings.Split(string(line), " ") {
+			addBigramToProbabilityMap(previousWord, word)
+			previousWord = word
+		}
+		addBigramToProbabilityMap(previousWord, "") //Add probability for ending on word
 		if isPrefix {
-			log.Println()
+			log.Println("Scentence too long")
+			//TODO Save the day
 		}
 		line, isPrefix, err = reader.ReadLine()
 	}
+}
+
+func addBigramToProbabilityMap(s1, s2 string) {
+	if bigramProbabilityMap[s1] == nil {
+		bigramProbabilityMap[s1] = make(map[string]int)
+	}
+	bigramProbabilityMap[s1][s2] += 1
+}
+
+func guessNextWord(str string) (bestGuess string, probability float64) {
+	max := -1
+	sum := 0
+	for word, occurances := range bigramProbabilityMap[str] {
+		if occurances > max {
+			max = occurances
+			bestGuess = word
+		}
+		sum += occurances
+	}
+	probability = float64(max) / float64(sum)
+	return
 }
