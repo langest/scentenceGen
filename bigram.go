4cb16008e531771c64fc4d4fcf58cb29bdd76784
Daniel Langesten <daniel.langest@gmail.com>
2018-10-07 11:13:44 +0200

    Adding some random to the generation

--- bigram.go
+++ bigram.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"bytes"
+	"log"
+	"math/rand"
 	"strings"
+	"time"
+	"errors"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 var (
 	bigramProbabilityMap map[string]map[string]int = make(map[string]map[string]int)
 )
 
 func addBigramToProbabilityMap(s1, s2 string) {
+	log.Println("Adding bigram probability:", s1, s2)
 	s1 = strings.ToLower(s1)
 	s2 = strings.ToLower(s2)
 	if bigramProbabilityMap[s1] == nil {
@@ -18,24 +27,30 @@ func addBigramToProbabilityMap(s1, s2 string) {
 	bigramProbabilityMap[s1][s2] += 1
 }
 
-func guessNextWord(str string) (bestGuess string, probability float64) {
-	bestGuess = ""
-	max := -1
+func guessNextWord(str string) (string, error) {
+	log.Println("Guessing next word")
 	sum := 0
-	for word, occurances := range bigramProbabilityMap[str] {
-		if occurances > max {
-			max = occurances
-			bestGuess = word
-		}
-		sum += occurances
+	for _, occurance := range bigramProbabilityMap[str] {
+		sum += occurance
 	}
-	probability = float64(max) / float64(sum)
-	return
+	log.Println("A")
+	r := rand.Intn(sum)
+	log.Println("r:", r)
+
+	for word, occurance := range bigramProbabilityMap[str] {
+		log.Println(word,":", occurance)
+		r -= occurance
+		if r < 0 {
+			return word, nil
+		}
+	}
+	return "", errors.New("Error, code should not reach this point")
 }
 
 func greedyMostProbableScentence() string {
+	log.Println("Greedy probable scentence search")
 	var buffer bytes.Buffer
-	for word, _ := guessNextWord(""); word != ""; word, _ = guessNextWord(word) {
+	for word, _ := guessNextWord(""); word != ""; word, _ = guessNextWord(word) { //TODO handle error
 		buffer.WriteString(word)
 		buffer.WriteString(" ")
 	}
--- main.go
+++ main.go
@@ -5,21 +5,32 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"gopkg.in/neurosnap/sentences.v1"
+	"gopkg.in/neurosnap/sentences.v1/data"
 )
 
 func main() {
+	b, _ := data.Asset("data/english.json")
+	training, _ := sentences.LoadTraining(b)
+	tokenizer := sentences.NewSentenceTokenizer(training)
+
 	reader := bufio.NewReader(os.Stdin)
 	line, isPrefix, err := reader.ReadLine()
 	for err == nil {
+		log.Println("Parsing line:\n", line)
 		previousWord := ""
-		for _, word := range strings.Split(string(line), " ") {
-			addBigramToProbabilityMap(previousWord, word)
-			previousWord = word
-		}
-		addBigramToProbabilityMap(previousWord, "") //Add probability for ending on word
-		if isPrefix {
-			log.Println("Scentence too long")
-			//TODO Save the day
+		for _, sentence := range tokenizer.Tokenize(string(line)) {
+			log.Println("Parsing sentence:\n", sentence)
+			for _, word := range strings.Split(sentence.Text, " ") {
+				addBigramToProbabilityMap(previousWord, word)
+				previousWord = word
+			}
+			addBigramToProbabilityMap(previousWord, "") //Add probability for ending on word
+			if isPrefix {
+				log.Println("Scentence too long")
+				//TODO Save the day
+			}
 		}
 		line, isPrefix, err = reader.ReadLine()
 	}
